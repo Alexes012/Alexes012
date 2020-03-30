350933a0d3a681f8c1167870335cc21c60ce8627
Alexei <61819719+Alexes012@users.noreply.github.com>
2020-03-30 16:36:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import s from './styles.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class counter extends React.Component {
+
+    state = {
+        startValue: 0,
+        maxValue: 5,
+        currentValue: 0
+    };
+
+
+    click = () => {
+        if (this.state.currentValue < this.state.maxValue) {
+            this.setState({currentValue: this.state.currentValue + 1})
+        }
+    };
+
+    resest = () => {
+        if (this.state.currentValue) {
+            this.setState({currentValue: this.state.startValue})
+        }
+    };
+
+
+    render = () => {
+
+        let disabled = this.state.currentValue === this.state.maxValue ? true:false;
+        let changecolor = this.state.currentValue === this.state.maxValue ? s.color : '';
+
+        return (
+            <div className={s.container}>
+                <div className={s.block}>
+                    <div className={s.span}>
+                        <div className={changecolor}>{this.state.currentValue}</div>
+                    </div>
+                    <div>
+                        <button onClick={this.click} disabled={disabled}>CLICK</button>
+                        <button onClick={this.resest}>RESET</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default counter;
---
+++ src/counter.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import s from './styles.module.css';
+
+
+class counter extends React.Component {
+
+  state = {
+    startValue: 0,
+    maxValue: 5,
+    currentValue: 0
+  };
+
+
+  click = () => {
+    if (this.state.currentValue = this.state.startValue) {
+      this.setState({currentValue: this.state.currentValue + 1})
+    }
+  };
+
+  resest = () => {
+    if (this.state.currentValue) {
+      this.setState({currentValue: this.state.startValue})
+    }
+  };
+
+
+  render = () => {
+
+    let disabled = this.state.currentValue === this.state.maxValue ? true:false;
+    let changecolor = this.state.currentValue === this.state.maxValue ? s.color : '';
+
+    return (
+        <div className={s.container}>
+          <div className={s.block}>
+            <div className={s.span}>
+              <div className={changecolor}>{this.state.currentValue}</div>
+            </div>
+            <div>
+              <button onClick={this.click} disabled={disabled}>INC</button>
+              <button onClick={this.resest}>RESET</button>
+            </div>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default counter;
